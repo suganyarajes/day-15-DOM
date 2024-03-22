676c4acb295f34976af7e27fb6adfa420372a74d
suganya <154896121+suganyarajes@users.noreply.github.com>
2024-03-21 19:15:04 -0500

    script.js

--- script.js
+++ script.js
@@ -9,10 +9,7 @@ function display(value) {
 
 // This function evaluates the expression and returns the result
 function calculate() {
-
-  setTimeout(function() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
-  }, 1000); // Delay execution for 1000 milliseconds (1 second)
-}
+  var p = document.getElementById("result").value;
+  var q = eval(p);
+  document.getElementById("result").value = q;
+} 
